3101b8a4d224ff3e11a2ce79b5cb0786a865824c
ster <ster.physics@gmail.com>
2022-11-05 11:11:43 +0900

    Version bump to v1.1.0

--- compass/compass/__init__.py
+++ compass/compass/__init__.py
@@ -10,7 +10,7 @@ __title__ = "compass"
 __author__ = "ster"
 __license__ = "GPL-3.0"
 __copyright__ = "Copyright 2021-present ster"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = (
     "Activation",
